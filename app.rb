65f894c0df68bdfb31084f331139f3adedf6e3ff
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-02-29 12:43:39 -0500

    added team names to path

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require "sinatra"
 require_relative "models/player"
 require_relative "models/team"
 
+team_data = TeamData.to_h
+team_names = team_data.keys
+
 get "/" do
   "<h1>The LACKP Homepage<h1>"
+
+end
+
+get "/teams" do
+  @team_names = team_names
+  erb :layout
 end
