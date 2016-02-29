7046ab92bcaf8751d04900b37723b197cd5d3884
Michael Hebert <michaelhebert@Michaels-MacBook-Pro.local>
2016-02-29 15:35:58 -0500

    got links to work, listed players

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
 require "sinatra"
 require_relative "models/player"
 require_relative "models/team"
-
-team_data = TeamData.to_h
-team_names = team_data.keys
+require 'pry'
 
 get "/" do
   "<h1>The LACKP Homepage<h1>"
@@ -11,11 +9,12 @@ get "/" do
 end
 
 get "/teams" do
-  @team_names = team_names
-  erb :layout
+  @teams = TeamData.to_h.keys
+  erb :index
 end
 
 get "/teams/:team_name" do
-  @team_name = params[:team_name]
-  erb :layout
+  binding.pry
+  @team = TeamData.to_h.select { |key| key == params[:team_name]}.first
+  erb :show_team
 end
--- models/team.rb
+++ models/team.rb
@@ -1,16 +1,5 @@
 require_relative "../lib/team_data"
 
 class Team
-  attr_reader :team_data
-
-  def initialize(team_data)
-    @team_data = team_data
-  end
-
-  def each_team
-    team_data.each_key do |team_name|
-      team_name
-    end
-  end
 
 end
